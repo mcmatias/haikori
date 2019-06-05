9a6dad74f385d73eeeb69d5bc7de99dac5a84e76
Gabriel Pontes <33611916+ograndoptimist@users.noreply.github.com>
2019-06-05 19:14:41 -0300

    O jogo Hairoki é executado aqui.

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
-#include "table.h"
+#include "tabuleiro.h"
 
-
-
+/*
+    Função que executa o jogo Hairoki.
+ */
 int main(int argc, char* argv[])
 {
-	char table_inicial[LINHA][COLUNA] = {{'I', 'D', 'D', 'I'},
-					     {'I', 'D', 'D', 'I'},
-					     {'I', 'I', 'I', 'I'},
-					     {'I', 'I', 'I', 'I'},
-					     {'I', '0', '0', 'I'}};
+	char tabuleiro_inicial[LINHA][COLUNA] = {{'I', 'D', 'D', 'I'},
+					         {'I', 'D', 'D', 'I'},
+					         {'I', 'I', 'I', 'I'},
+					         {'I', 'I', 'I', 'I'},
+					         {'I', '0', '0', 'I'}};
 	
 	int livre1[] = {CORINGA, CORINGA};
 	int livre2[] = {CORINGA, CORINGA};
 	
-	printTable(table_inicial);
+	imprimeTabuleiro(tabuleiro_inicial);
 	
-	retornaCasaVazia(table_inicial, livre1, livre2);
+	retornaCasaVazia(tabuleiro_inicial, livre1, livre2);
 	
 	printf("LIVRE1: %d%d\n", livre1[0], livre1[1]);
 	printf("LIVRE2: %d%d\n", livre2[0], livre2[1]);
