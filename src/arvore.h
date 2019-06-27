44c146c2d96404a4085d86a457d10bf4670a774f
Flavia Santo <flavia.santo@tvglobo.com.br>
2019-06-27 20:59:06 -0300

    Nova_arvore_nao_binaria

--- src/arvore.h
+++ src/arvore.h
@@ -1,155 +1,158 @@
+/*
+   Arquivo que fornece a estrutura de dados base para o tratamento do problema.
+   Aqui é implementada um árvore que pode apresentar até no máximo 6 filhos.
+   Funções públicas que foram desenvolidas para interagir com a interface da estrutura de dados:
+	  1> search_item(item)
+          2> insert_item(item)
+*/
 #include <stdio.h>
-#iclude "tabuleiro.h"
+#include <stdbool.h>
+#include "tabuleiro.h"
 
-#define TAM_TABULEIRO 50
 
-struct Arvore
+/* Definição dos protótipos das funções presentes neste arquivo */
+Node *choose_node(Node *node);
+bool insert_node(Node *root, Node *node_new);
+Node *search_node(Node *root, char tabuleiro_configuracao[]);
+Node *create_node(void);
+
+
+/*
+   Construção do tipo Abstrato de dados: um nó que pode ter até 6 nós filhos.
+ */
+typedef struct Node {
+	struct Node *child_1 = NULL;
+	struct Node *child_2 = NULL;
+	struct Node *child_3 = NULL;
+	struct Node *child_4 = NULL;
+	struct Node *child_5 = NULL;
+	struct Node *child_6 = NULL;
+	char configuracao_tabuleiro[LINHA][COLUNA]	
+} Node;
+
+
+Node *root;
+
+
+/*
+   Função que auxilia na inserção de novos nós.
+   Esta função retorna o endereço de um nó filho disponível entre os 6.
+   Ela inicia o checking de disponibilidade a partir do primeiro filho,
+   portanto, os nós não apresentam nenhum tipo de ordenação.
+*/
+Node *choose_node(Node *node) 
 {
-	struct Arvore* subdireita;
-	struct Arvore* subesquerda;
-	struct Arvore* novo;
-} 
+	Node *result = NULL;
 
-// Função que insere dados na árvore recursivamente
-void inserirarvore(Arvore** raiz, Arvore** novo) 
-{
-
-	if(*raiz == NULL)
+	if((result = node.child_1) == NULL)
 	{
-		*raiz = (struct Arvore*)malloc(sizeof(Arvore)); 
-    
-		if(!raiz)
-		{
-			printf("Erro ao alocar =(\n");
-			exit(1);
-		}
-    
-    *raiz = *novo
-    (*raiz)->subesquerda = NULL; 
-    (*raiz)->subdireita = NULL; 
- 
-	} 
-	else
+		;
+	} else if((result = node.child_2) == NULL)
 	{
-		if((*novo)->hash < (*raiz)->hash) {
-			inserirarvore(&(*raiz)->subesqueda, novo);
-		}
-		else { 
-			inserirarvore(&(*raiz)->subdireita, novo);
-		}
+		;
+	} else if((result = node.child_3) == NULL)
+	{
+		;
+	} else if((result = node.child_4) == NULL)
+	{
+		;
+	} else if((result = node.child_5) == NULL)
+	{
+		;
+	} else if((result = node.child_6) == NULL)
+	{
+		;
 	}
+
+	return result;
 }
 
-// Função de busca na arvore
 
-struct arvore* buscararvore(struct arvore **raiz, int hash)
+/*
+    Função que cria um novo nó.
+*/
+Node *create_node(void)
 {
-	if (*raiz == NULL)
+	Node *node_new = (Node*) malloc(sizeof(Node));
+	return node_new
+}
+
+
+/*
+   Função que insere novos nós numa árvore de nós, de forma recursiva.   
+*/
+bool insert_node(char configuracao_tabuleiro[][]) 
+{
+
+	if(root->configuracao_tabuleiro == NULL)
+	{
+		root->configuracao_tabuleiro = configuracao_tabuleiro;
+	} else 
+	{
+		Node *node_new = create_node;
+		node_new = insert_node(choose_node(parent), node_new);
+		return true;		
+	}			
+}
+
+
+/*
+   Função que realiza busca de nós numa árvore.
+   Encontrado o nó em questão, a função retorna o seu endereço.
+   A busca é realizada sempre dós nós filhos mais a esquerda para a direita.   
+*/
+Node *search_node(Node *node, char tabuleiro_configuracao[][])
+{
+	if (node == NULL)
 	{
 		return NULL;
-	}
-	else
+	} else if(node->configuracao_tabuleiro == configuracao_tabuleiro)
 	{
-		if ( hash < (*raiz)->hash){ 
-			return buscararvore(&(*raiz)->subesquerda, hash);
-		}
-		else if (hash > (*raiz)->hash) {
-			return buscararvore(&(*raiz)->subdireita, hash);
-		}
-		else {
-		return *raiz;
-		}
-	}
-}
-
-int criarnovapossibilidade (char tabuleiro[LINHA][COLUNA], Arvore **novono)
-{
-	int i;
-	int j = 0;
-	int k = 0;
-	int existente = 0;
-	int colisao = 0;
-	struct Arvore* existe; 
-
-	int hs = hash(tabuleiro);
-	existe = buscararvore(&raiz, hs);
-	
-	// Função que verifica colisões
-	
-	if (existe != NULL)
+		return node;
+	} else 
 	{
-		for (j=0; j<LINHA; j++)
+		if(node->child_1 != NULL && node->child_1->configuracao_tabuleiro != configuracao_tabuleiro)
 		{
-			for (k=0; j<COLUNA; k++)
-			{
-				if (tabuleiro[j][k] != existe->tabuleiro[j][k])
-				{
-					colisao = 1;
-					break
-				}
-			}
+			return search_node(node->child_1, tabuleiro_configuracao);
 		}
-		existente = colisao? 0 : 1;
-	}
-	
-	// Função que adiciona novo possivel no
-	
-	if(!existente)
-	{
-		for i=0; i<8; i++)
+		
+		if(node->child_2 != NULL node->child_2->configuracao_tabuleiro != configuracao_tabuleiro)
 		{
-			if((*novono)->possibilidades[i] == NULL)
-			{
-				(*novono)->possibilidades[i] = (struct Arvore*)malloc(sizeof(Arvore));
-				for (j=0; j<LINHA; j++)
-				{
-					for (k=0; j<COLUNA; k++)
-					{
-						(*novono)->possibilidades[i]->tabuleiro[j][k] = tabuleiro[j][k];
-					}
-				}
-			(*novono)->possibilidades[i]->hash = hs;
-			inserirarvore(*raiz, &((*novono)->possibilidades[i]));
-			break;
-			}
-		}
-	}
-	return 1;
-}
-
-int hash(char tabuleiro[LINHA][COLUNA])
-{
-	int i=0;
-	int j=0;
-	int k=0;
-
-	char s[TAM_TABULEIRO];
-
-	for (i=0; i<LINHA; i++)
-	{
-		for (j=0; j<COLUNA; j++)
+			return search_node(node->child_2, tabuleiro_configuracao);
+		} 
+		
+		if(node->child_3 != NULL node->child_3->configuracao_tabuleiro != configuracao_tabuleiro)
 		{
-			s[k] = tabuleiro[i][j];
-			k++;	
+			return search_node(node->child_3, tabuleiro_configuracao);
 		}
-
-	}
-	s[k] = '\0';
-	return fnv_hash(s);
-}
-
-unsigned int fnv_hash (const char *s)
-{
-	register unsigned int i;
  
-	for (i = 0; *s; s++)
-    {
-		i *= 16777619;
-		i ^= *s;
-    }
+		if(node->child_4 != NULL node->child_4->configuracao_tabuleiro != configuracao_tabuleiro)
+		{
+			return search_node(node->child_4, tabuleiro_configuracao);
+		}
 
-	 return i;
-  
- //Para mais detalhes sobre fnv_hash: http://www.isthe.com/chongo/tech/comp/fnv/index.html#FNV-param
- 
+		if(node->child_5 != NULL node->child_5->configuracao_tabuleiro != configuracao_tabuleiro)
+		{
+			return search_node(node->child_5, tabuleiro_configuracao);
+		}
+
+		if(node->child_6 != NULL node->child_6->configuracao_tabuleiro != configuracao_tabuleiro)
+		{
+			return search_node(node->child_6, tabuleiro_configuracao);
+		}
+	}		
+}
+
+
+/*
+ */
+bool  search_item(char configuracao_tabuleiro[][])
+{
+	if((Node *node = search_node(root)) != NULL)
+	{
+		return true;
+	} else
+	{
+		return false;
+	}	
 }
